aee85217adb10123301d8d009720035bba85fab0
Tayleron <tayleron@gmail.com>
2019-10-07 10:50:58 -0230

    began quickstart project 1

---
+++ quickstart/src/IdentityServer/Config.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new ApiResource[] 
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] 
+            {
+                new Client 
+                {
+                    ClientId = "client",
+                    //no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    //secret for authentication
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    //scopes that sliant has access to
+                    AllowScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
